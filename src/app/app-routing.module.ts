97cebfeae23d0c30e0ea2201bc94ef469fe25ca8
Aifia Mohamed <mohamedaifia34@gmail.com>
2023-09-13 02:56:11 +0100

    routing/tailwind-config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "" , redirectTo: "home", pathMatch: "full"},
+  {path: "home", data: {breadcrumb:"Home"}, component: HomeComponent },
+  {path: "portfolio", data: {breadcrumb:"Portfolio"}, component: PortfolioComponent},
+  {path: "about", data: {breadcrumb:"About"}, component: AboutComponent},
+  {path: "skills", data: {breadcrumb:"Skills"}, component: SkillsComponent},
+  {path: "contact", data: {breadcrumb:"Contact"}, component: ContactComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    SkillsComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
