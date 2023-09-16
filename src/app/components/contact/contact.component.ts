f1a81c8049ec2357ea2a53c8cccb4a534521d7f9
Aifia Mohamed <mohamedaifia34@gmail.com>
2023-09-16 04:47:34 +0100

    portfolio v1

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  form : FormGroup;
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group({
+      name: [''],
+      email: [''],
+      message: ['']
+    })
+  }
+  onSubmit(event: Event){
+    event.preventDefault();
+
+    window.location.reload();
+  }
 
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  
+
  menu :boolean =false;
 
  openMenu(){
